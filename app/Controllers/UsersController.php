e09ba386342599aea9e7b2622c590de755b5b3d0
Matheus Souza <matheusviegasdesouza@gmail.com>
2018-05-31 13:25:00 -0300

    Criando o cadastro e edição de usuários

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -7,8 +7,10 @@ use App\Core\Helpers\Upload;
 use App\Core\Helpers\Email;
 
 use App\Models\Usuario;
+use App\Models\Grupo;
 use App\Core\Auth;
 use App\Core\Helpers\Session;
+use App\Core\Helpers\Input;
 
 class UsersController extends Controller {
 
@@ -32,6 +34,43 @@ class UsersController extends Controller {
       ];
 
         $this->loadTemplate('Users/users_list', ['usuarios' => Usuario::all()], $configTemplate);
+    }
+
+    public function add(){
+      $configTemplate = [
+        'titulo' => 'Usuários', 
+        'titulo_panel' => 'Cadastrar Usuário'
+      ];
+
+        $this->loadTemplate('Users/users_add', ['grupos' => Grupo::all()], $configTemplate);
+    }
+
+    public function edit($id){
+      $configTemplate = [
+        'titulo' => 'Usuários', 
+        'titulo_panel' => 'Editar Usuário'
+      ];
+
+        $this->loadTemplate('Users/users_add', ['grupos' => Grupo::all(), 'usuario' => Usuario::find(intval($id))], $configTemplate);
+    }
+
+    public function save(){
+      $dados = Input::post();
+
+      $usuario = !empty($dados['id']) ? Usuario::find(intval($dados['id'])) : new Usuario();
+      $usuario->nome = $dados['nome'];
+      $usuario->sobrenome = $dados['sobrenome'];
+      $usuario->email = $dados['email'];
+      $usuario->usuario = $dados['usuario'];
+      $usuario->grupoid = $dados['grupo'];
+
+      if(!empty($dados['senha']) && !empty($dados['confirmacao_senha']) && $dados['senha'] == $dados['confirmacao_senha']){
+        $usuario->senha = MD5($dados['senha']);
+      }
+
+      $usuario->save();
+
+      $this->redirect('users/add', ['flash' => ['success' => 'Alterações salvas com sucesso!']]);
     }
 
     public function teste(){
---
+++ app/Views/Users/users_add.php
@@ -0,0 +1,63 @@
+<div class="row">
+	<div class="col-md-6">
+		<form method="POST" action="<?=$this->base('users/save');?>">
+			<?php if(!empty($usuario)) : ?>
+				<input type="hidden" name="id" value="<?=$usuario->id;?>" />
+			<?php endif; ?>
+
+			<div class="row">
+				<div class="col-md-4">
+					<input type="text" class="form-control" placeholder="Nome" name="nome" required value="<?=(!empty($usuario) ? $usuario->nome : '');?>" />
+				</div>
+				<div class="col-md-8">
+					<input type="text" class="form-control" placeholder="Sobrenome" name="sobrenome" required value="<?=(!empty($usuario) ? $usuario->sobrenome : '');?>" />
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<input type="email" class="form-control" placeholder="Email" name="email" required value="<?=(!empty($usuario) ? $usuario->email : '');?>" />
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6">
+					<input type="text" class="form-control" placeholder="Nome de Usuário" name="usuario" required value="<?=(!empty($usuario) ? $usuario->usuario : '');?>" />
+				</div>
+				<div class="col-md-6">
+					<select class="form-control" name="grupo">
+						<option>Selecionar Grupo</option>
+						<?php foreach($grupos as $grupo): ?>
+							<option value="<?=$grupo->id;?>" <?=(!empty($usuario) && $usuario->grupoid == $grupo->id ? 'selected' : '');?>><?=$grupo->nome;?></option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6">
+					<input type="password" class="form-control" placeholder="Senha" name="senha" />
+				</div>
+				<div class="col-md-6">
+					<input type="password" class="form-control" placeholder="Repita a Senha" name="confirmacao_senha" />
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-md-6">
+					<label class="btn btn-default" style="width: 100%;">
+					    <i class="fa fa-camera" style="margin-right: 5px;"></i> Selecionar Foto <input type="file" style="display: none;">
+					</label>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<input type="submit" name="salvar" value="Salvar Alterações" class="btn btn-primary" /> <a href="<?=$this->base('users');?>" class="btn btn-default">Voltar</a>
+				</div>
+			</div>
+					
+		</form>
+	</div>
+</div>
