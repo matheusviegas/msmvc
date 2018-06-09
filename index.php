3f9ad6eff5b9d502b8bba542166f3f16c8b5cf4e
Matheus Souza <matheusviegasdesouza@gmail.com>
2018-06-09 10:08:06 -0300

    Corrigindo requerimento do preenchimento da variavel de ambiente BASE_URL, já que não será mais definida no arquivo .env

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
-$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_DRIVER', 'DB_TIMEZONE', 'APP_TIMEZONE', 'APP_KEY', 'DB_CHARSET', 'DB_COLLATION', 'BASE_URL'])->notEmpty();
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_DRIVER', 'DB_TIMEZONE', 'APP_TIMEZONE', 'APP_KEY', 'DB_CHARSET', 'DB_COLLATION'])->notEmpty();
 $dotenv->required('DB_PORT')->isInteger();
 $dotenv->required('ENVIRONMENT')->allowedValues(['development', 'production']);
 $dotenv->required('DB_PASSWORD');
