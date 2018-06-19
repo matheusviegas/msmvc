6f703888954117aa8c72c867d8a6bb57197e9085
Matheus Souza <matheusviegasdesouza@gmail.com>
2018-06-18 21:00:39 -0300

    Adicionando a opção de definir o status da response através do helper response(status, texto=null)

--- App/Helpers/Response.php
+++ App/Helpers/Response.php
@@ -4,8 +4,8 @@ use App\Core\Http\Response;
 
 if(!function_exists('response')){
 
-	function response() {
-		return new Response();
+	function response($statusCode = 200, $text = null) {
+		return new Response($statusCode, $text);
 	}
 
 }
