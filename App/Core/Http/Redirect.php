454e9c7205b22df7cb5f5483167145f5d540e8e9
Matheus Souza <matheus@mertins.com.br>
2018-06-19 12:09:48 -0300

    Criando possibilidade de chamar os metodos dinamicos success, error, info e warning no redirecionamento usando o método magico __call

--- App/Core/Http/Redirect.php
+++ App/Core/Http/Redirect.php
@@ -5,11 +5,13 @@ namespace App\Core\Http;
 class Redirect {
 
 	private $url;
+	private $allowedFunctions;
 
 	public function __construct($url = null) {
 		if($url != null){
 			$this->url = $this->checkURL($url);
 		}
+		$this->allowedFunctions = ['success', 'error', 'info', 'warning'];
 	}
 
 	public function with($key, $val = 'Invalid') {
@@ -37,5 +39,17 @@ class Redirect {
 	private function checkURL($url) {
 		return preg_match('/http(s?)\:\/\//i', $url) ? $url : BASE_URL . $url;
 	}
+
+	public function __call($function, $args) {
+		if(in_array($function, $this->allowedFunctions)){
+			foreach($args as $msg) {
+				$this->with($function, $msg);
+			}
+		} else {
+			throw new \Exception("{$function} não é um método permitido.");
+		}
+
+		return $this;
+    }
 
 }
